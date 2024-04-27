5ef62f151ffb913b23d4775b75931b1e641b3450
mattb00ker <mattbooker@hotmail.com>
2024-04-27 16:54:11 +0100

    cleaning code

--- 2Dliststictactoe.py
+++ 2Dliststictactoe.py
@@ -1,6 +1,5 @@
 board = [[0,0,0],[0,0,0],[0,0,0]]
 
-global isWinner
 isWinner = False
 
 def showBoard():
@@ -12,32 +11,30 @@ def xTurn():
     col = input("it's X's turn, which column?")
     row = input("and which row?")
     board[int(row)][int(col)] = "x"
+    checkForWin("x")
 
 def yTurn():
     col = input("it's Y's turn, which column?")
     row = input("and which row?")
     board[int(row)][int(col)] = "y"
+    checkForWin("y")
 
 def checkForWin(winner):
-
     #horizontal
-    if board[0][0] == board[0][1] == board[0][2] or board[1][0] == board[1][1] == board[1][2] or board[2][0] == board[2][1] == board[2][2]:
+    if board[0][0] == board[0][1] == board[0][2] == winner or board[1][0] == board[1][1] == board[1][2] == winner or board[2][0] == board[2][1] == board[2][2] == winner:
         print(winner, " is the winner!")
     #diagonal
-    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+    if board[0][0] == board[1][1] == board[2][2] == winner or board[0][2] == board[1][1] == board[2][0] == winner :
         print(winner, " is the winner!")
     #vertical
-    if board[0][0] == board[1][0] == board[2][0] or board[0][1] == board[1][1] == board[2][1] or board[0][2] == board[1][2] == board[2][2]:
+    if board[0][0] == board[1][0] == board[2][0] == winner or board[0][1] == board[1][1] == board[2][1] == winner or board[0][2] == board[1][2] == board[2][2] == winner :
         print(winner, " is the winner!")
-        
+
 while isWinner == False:
     showBoard()
     xTurn()
-    checkForWin("x")
     showBoard()
     yTurn()
-    checkForWin("y")
-
 
 
 
