6eff54bcba1f9dff0261a68291e5bc7d0532a997
mattb00ker <mattbooker@hotmail.com>
2024-09-27 12:05:38 +0100

    working to get tkinter firing

--- pySnake.py
+++ pySnake.py
@@ -3,7 +3,7 @@ from tkinter import *
 import random
 
 GAME_WIDTH = 700
-GAME_HEIGH = 700
+GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
@@ -33,5 +33,15 @@ window = Tk()
 window.title("Snek Game")
 window.resizable(False, False)
 
+score = 0 
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOUR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
 
 window.mainloop()
