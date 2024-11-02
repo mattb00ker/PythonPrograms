6303468c9d0070580210071d0596c2e2904fe64e
mattb00ker <mattbooker@hotmail.com>
2024-11-02 22:24:46 +0000

    working thru the course

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from room import Room
 from item import Item
+from character import Character
+from character import Enemy
 
 #Building rooms
 kitchen = Room("Kitchen")
@@ -28,6 +30,11 @@ kitchen.add_items(sword)
 #dining_hall.get_details()
 #kitchen.get_details()
 #ballroom.get_details()
+
+dave = Enemy("Dave", "A smelly zombie")
+dave.describe()
+dave.set_conversation("I love Spurs")
+dave.talk()
 
 current_room = kitchen          
 
