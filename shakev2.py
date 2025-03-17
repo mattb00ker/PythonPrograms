cb2a9ebe98941e223bf7ba446f216f063ecc68a5
Matt Booker <73802648+mattb00ker@users.noreply.github.com>
2025-03-17 18:55:28 +0000

    considering getting back to some ai

---
+++ shakev2.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import platform
+import time
+import pathlib
+import os
+
+print('Python version:', platform.python_version())
+print('Tensorflow version:', tf.__version__)
+print('Keras version:', tf.keras.__version__)
